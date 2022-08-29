3ae9c22b96d2cbae89f803fa773fea07b4e763c2
Gabriel <gritegabriel@gmail.com>
2022-08-28 21:37:24 -0300

    75% da index refeita e responsiva

---
+++ app.js
@@ -0,0 +1,29 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
