8f8cea59745b9be79eae99b801b7dbd0f402b21f
Gustavo Ramalho <gugarroliver@hotmail.com>
2022-10-26 21:16:12 -0300

    Add AboutPage

--- like-a-mozart/src/components/Pages/AboutPage/index.js
+++ like-a-mozart/src/components/Pages/AboutPage/index.js
@@ -1,5 +1,24 @@
+import image from "../../../images/undraw_off_road_re_leme.svg"
+
 const AboutPage = () => {
-  return <div>about</div>;
+  return <div>
+    <h2>About Us:</h2>
+    <div class="About">
+        <img src={image}/>
+        <div class="About_content">
+            <p>Like a Mozart foi criado pensando na dificuldade de se comprar instrumentos
+                musicais e equipamentos de estúdio e gravação de áudio profissionais no Brasil, pois além de
+                preços exorbitantes, a variedade é um fator decisivo na hora de comprar instrumentos. Portanto contamos
+                com uma equipe capacitada em identificar o melhor instrumento pelo melhor preço possível,
+                contanto com uma equipe técnica responsável pela avaliação inicial do instrumento, apontando os defeitos
+                para que nossos luthiers de plantão possa deixá-lo na melhor condição para que desde o
+                estudante até o profissional possa tirar o melhor som do instrumento.</p>
+            <p >Por conta de grandes parcerias firmadas diretamente com os grandes fabricantes
+                de instrumentos nacionais, conseguimos proporcionar o melhor preço do mercado para garantir que a música
+                esteja presente em todos os lares brasileiros.</p>
+        </div>
+    </div>
+  </div>;
 };
 
 export default AboutPage;
