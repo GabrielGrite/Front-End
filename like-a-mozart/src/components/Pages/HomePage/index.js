cf277096f6f7f2b28bb71372fc0ca0eade5934cc
Gustavo Ramalho <gugarroliver@hotmail.com>
2022-10-26 21:10:12 -0300

    Updates

--- like-a-mozart/src/components/Pages/HomePage/index.js
+++ like-a-mozart/src/components/Pages/HomePage/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
-import secondaryBanner from "../../../images/secondary-banner-placeholder.jpg";
+import bannerImage1 from "../../../images/bg1.jpg";
+import bannerImage2 from "../../../images/bg2.jpg";
+import bannerImage3 from "../../../images/bg3.jpg";
+
 import undrawOffRoadReLeme from "../../../images/undraw_off_road_re_leme.svg";
 import merchantImage from "../../../images/undraw_compose_music_re_wpiw.svg";
 import Slideshow, { SlideBanner } from "../../ui/Slideshow";
@@ -9,8 +12,9 @@ const HomePage = () => (
     <div className="main__container">
       <div className="main__content">
         <Slideshow>
-          <SlideBanner img={secondaryBanner} title="Destaques da semana" />
-          <SlideBanner img={secondaryBanner} title="Destaques da semana" />
+          <SlideBanner img={bannerImage1} title="Destaques da semana" />
+          <SlideBanner img={bannerImage2} title="Destaques da semana" />
+          <SlideBanner img={bannerImage3} title="Destaques da semana" />
         </Slideshow>
         <br />
         <hr />
@@ -21,24 +25,25 @@ const HomePage = () => (
           <h2>Like a Mozart:</h2>
           <hr />
           <p>
-            Buscamos trazer as ferramentas necessarias para você compositor,
-            criar desde a mais simples melodia ate a melhor sinfonia que ja
-            ouvimos
+            Nosso objetivo é trazer as ferramentas necessárias para você,
+            compositor e produtor musical, criar desde a mais simples melodia
+            ate a melhor sinfônia que já ouvimos.
           </p>
           <p>
-            Nossos instrumentos são das mais diversas regioes,tipos e
-            variedades.Tentando sempre chegar o mais proximo do instrumento do
-            futuro musicista
+            Nossos instrumentos são das mais diversas regiões, da maior
+            variedade e qualidade encontrada, tentando sempre chegar o mais
+            próximo do instrumento do futuro musicista
           </p>
           <p>
-            Trazemos produtos de qualidade por um preço mais em conta comparado
-            aos concorrentes
+            Trazemos produtos de qualidade por um preço mais acessível em
+            comparação com o restante do mercado, pois o nosso maior objetivo, é
+            levar a música até você.
           </p>
         </div>
       </div>
       <hr />
       <div className="merchant__image__container">
-        <div className="merchant__content">
+        <div class="merchant__content">
           <h2>Sua diversão é musica para nossos ouvidos.</h2>
           <hr />
           <p>
---
+++ like-a-mozart/src/components/Pages/ProductsPage/ProductItem/index.js
@@ -0,0 +1,30 @@
+import "./style.css";
+import placeholderImage from "../../../../images/secondary-banner-placeholder.jpg"
+import { formatMoney } from "../../../../lib/money-utils";
+
+
+const ProductItem = ({ product }) => {
+  const { name, type, value } = product;
+
+  return (
+    <div class="product-item">
+      <div class="product-img">
+        <img
+          src={placeholderImage}
+          alt="product image"
+        />
+        <button type="button" class="add-to-cart-btn">
+          <i class="fas fa-shopping-cart"></i>Add To Cart
+        </button>
+      </div>
+      <div class="product-content">
+        <h3 class="product-name">{name}</h3>
+        <span class="product-category">Tipo: {type}</span>
+        <p class="product-price">{formatMoney(value)}</p>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default ProductItem;
